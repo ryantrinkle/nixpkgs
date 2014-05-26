de6f14c057a3604b288f1e1f982b51f1eefa7293
John Wiegley <johnw@newartisans.com>
2014-05-26 02:04:59 -0500

    create

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9869,6 +9869,9 @@ let
 
   crack_attack = callPackage ../games/crack-attack { };
 
+  crafty = callPackage ../games/crafty { fullVariant = false; };
+  craftyFull = callPackage ../games/crafty { fullVariant = true; };
+
   crrcsim = callPackage ../games/crrcsim {};
 
   dhewm3 = callPackage ../games/dhewm3 {};
