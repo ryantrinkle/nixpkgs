10552fb64e425fa7e3d1968c83ade891fbab9219
ali abrar <aliabrar@gmail.com>
2015-01-24 17:10:00 -0500

    ghcjs-dom xhr

--- pkgs/development/libraries/haskell/ghcjs-dom/default.nix
+++ pkgs/development/libraries/haskell/ghcjs-dom/default.nix
@@ -4,10 +4,11 @@ cabal.mkDerivation (self: {
   pname = "ghcjs-dom";
   version = "0.1.1.3";
   src = fetchgit {
-    url = git://github.com/ryantrinkle/ghcjs-dom.git;
-    rev = "90b52182ab3d2ca5cbed7b2fbb33fa06f280771e";
-    sha256 = "949a661516206252b36dc817c07ababb7f50a389661d838f0fee0be15085aff9";
+    url = git://github.com/xionite/ghcjs-dom.git;
+    rev = "2d5a0aa64454f2c084b1a7c53cadb59b274d1386";
+    sha256 = "8450b1a0de67cf6bb6c304c244e211331da8f5befdf92c089498c4394c14fcc2";
   };
+
   buildDepends = [ ghcjsBase mtl text ];
   meta = {
     description = "DOM library that supports both GHCJS and WebKitGTK";
