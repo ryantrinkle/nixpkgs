0a93c6ec2dcce65e87b4b26cd60efd88a9388535
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-04-21 05:21:36 -0400

    Get rid of unnecessary heist override

--- pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix
@@ -306,17 +306,6 @@ self: super: {
     license = pkgs.stdenv.lib.licenses.bsd3;
   }) {};
 
-  heist = overrideCabal super.heist (drv: {
-    sha256 = null;
-    src = pkgs.fetchgit {
-      url = git://github.com/ryantrinkle/heist;
-      rev = "b9f9427cf23747fbe38b318d4e4a468e9be23c31";
-      sha256 = "17641bffe7b5d4c2d585aad2f6ed8109047b1c6970ee790aa775df2ad2bf3d30";
-    };
-    editedCabalFile = null;
-    version = "0.14.0.2";
-  });
-
   base64-bytestring = dontCheck super.base64-bytestring; # Hangs
   vector-algorithms = dontCheck super.vector-algorithms; # Hangs
   snap = overrideCabal super.snap (drv: {
