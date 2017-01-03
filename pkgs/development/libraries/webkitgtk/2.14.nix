7125b64fff66c7757ccec2a6b01b11746bf50298
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-01-02 23:19:54 -0500

    webkitgtk214x: fix darwin
    Huge thanks to pikajude for his help on this commit, including the initialize-threading patch and -DCMAKE_BUILD_WITH_INSTALL_RPATH=1

--- pkgs/development/libraries/webkitgtk/2.14.nix
+++ pkgs/development/libraries/webkitgtk/2.14.nix
@@ -2,8 +2,8 @@
 , pkgconfig, gettext, gobjectIntrospection, libnotify, gnutls
 , gtk2, gtk3, wayland, libwebp, enchant, xlibs, libxkbcommon, epoxy, at_spi2_core
 , libxml2, libsoup, libsecret, libxslt, harfbuzz, libpthreadstubs
-, enableGeoLocation ? true, geoclue2, sqlite
-, gst-plugins-base
+, enableGeoLocation ? !stdenv.isDarwin, geoclue2, sqlite
+, gst-plugins-base, readline, libedit, mesa, libintlOrEmpty, darwin
 }:
 
 assert enableGeoLocation -> geoclue2 != null;
@@ -31,17 +31,34 @@ stdenv.mkDerivation rec {
 
   # see if we can clean this up....
 
-  patches = [ ./finding-harfbuzz-icu.patch ];
+  patches = [
+    ./finding-harfbuzz-icu.patch
+    ./initialize-threading.patch
+  ];
 
   cmakeFlags = [
-  "-DPORT=GTK"
-  "-DUSE_LIBHYPHEN=0"
-  "-DENABLE_GLES2=ON"
+    "-DPORT=GTK"
+    "-DUSE_LIBHYPHEN=0"
+  ] ++ (if stdenv.isDarwin then [
+    "-DENABLE_GEOLOCATION=OFF"
+    "-DENABLE_OPENGL=OFF"
+    "-DCMAKE_MACOSX_RPATH=1"
+    "-DCMAKE_BUILD_WITH_INSTALL_RPATH=1"
+  ] else [
+    "-DENABLE_GLES2=ON"
+  ]);
+
+  configureFlags = optionals stdenv.isDarwin [
+    "--disable-x11-target"
+    "--enable-quartz-target"
+    "--disable-web-audio"
   ];
 
   # XXX: WebKit2 missing include path for gst-plugins-base.
   # Filled: https://bugs.webkit.org/show_bug.cgi?id=148894
   NIX_CFLAGS_COMPILE = "-I${gst-plugins-base.dev}/include/gstreamer-1.0";
+
+  NIX_LDFLAGS = if stdenv.isDarwin then "-lintl" else "";
 
   nativeBuildInputs = [
     cmake perl python2 ruby bison gperf sqlite
@@ -49,11 +66,16 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    gtk2 wayland libwebp enchant libnotify gnutls
+    gtk2 libwebp enchant libnotify gnutls
     libxml2 libsecret libxslt harfbuzz libpthreadstubs
     gst-plugins-base libxkbcommon epoxy at_spi2_core
   ] ++ optional enableGeoLocation geoclue2
-    ++ (with xlibs; [ libXdmcp libXt libXtst ]);
+    ++ (with xlibs; [ libXdmcp libXt libXtst ])
+    ++ (if stdenv.isDarwin then [
+    readline libedit mesa libintlOrEmpty
+  ] else [
+    wayland
+  ]);
 
   propagatedBuildInputs = [
     libsoup gtk3
